5ffc94fdc1ba84d4ddd62cb42ed31c1d7c069e22
eliransc <scherzter@gmail.com>
2022-11-21 16:01:30 +0200

    Adding the new code

--- __main__.py
+++ __main__.py
@@ -24,7 +24,7 @@ def simulate_competition():
     results = []
     for i in range(3):
         now = time.time()
-        simulator = Simulator(  planner = my_planner, instance_file="BPI Challenge 2017 - instance 2.pickle") # running_time = running_time,
+        simulator = Simulator(  planner = my_planner, instance_file="BPI Challenge 2017 - instance.pickle") # running_time = running_time,
 
         if type(my_planner) == PPOPlanner:
             my_planner.linkSimulator(simulator)
