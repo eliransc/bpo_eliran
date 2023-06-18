97f700f1108fcd1bb8dc1f81918c5e8195e55eb5
eliransc <scherzter@gmail.com>
2023-06-18 15:16:46 +0300

    Adding the new code

--- main1.py
+++ main1.py
@@ -27,7 +27,7 @@ def simulate_competition(a1,a2,a3,a4,a5):
 
     my_planner = planner_Eliran(a1, a2, a3, a4, a5)
 
-    mod_num = np.random.raindint(1, 10000000)
+    mod_num = np.random.randint(1, 10000000)
 
     results = []
     for i in range(15):
