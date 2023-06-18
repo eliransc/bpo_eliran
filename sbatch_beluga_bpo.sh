c9ddd467fa8025d2948d2c307769472f69167bd9
eliransc <scherzter@gmail.com>
2023-06-18 14:51:33 +0300

    Adding the new code

--- sbatch_beluga_bpo.sh
+++ sbatch_beluga_bpo.sh
@@ -3,6 +3,6 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 10000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/bpo_eliran/__main__.py
+python /home/eliransc/projects/def-dkrass/eliransc/bpo_eliran/bayes_opt.py
 
 
